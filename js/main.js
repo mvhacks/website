dba5b4b3449f4a248c50b5906305eef412e30784
Arjun Patrawala <ajpat1234@gmail.com>
2019-04-26 01:41:52 -0700

    partner and sponsor stuff

--- js/main.js
+++ js/main.js
@@ -58,3 +58,57 @@ if (navigator.serviceWorker) {
 		}
 	});
 }
+
+
+/* do all the sheet stuff */
+let sheetId = '191YLtCW0myhV1qoNfudca9MaiqTTVDRPsm8YZWBTkRU';
+let apiKey = 'AIzaSyDEBLKN17IKxy0IsigeP4XB6ivTh-dGRac';
+
+fetch(
+	`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values:batchGet?ranges=Sponsors&ranges=Schedule&key=${apiKey}`
+).then(res => res.json()).then(res => {
+	let ranges = res.valueRanges;
+
+	for (let i = 0; i < ranges.length; i++) {
+		if (ranges[i].range.includes('Sponsors')) {
+			handleSponsorData(ranges[i].values);
+		} else if (ranges[i].range.includes('Schedule')) {
+			handleScheduleData(ranges[i].values);
+		}
+	}
+});
+
+function handleSponsorData(data) {
+	let sponsorHTML = '';
+	let partnerHTML = '';
+
+	for (let i = 1; i < data.length; i++) {
+		let val = data[i];
+
+		if (val[0] === 'Partner') {
+			partnerHTML += `
+				<a href="${val[3]}" target="_blank" class="sponsor-image partner-image">
+					<div>
+						<img alt="${val[1]}" src="${val[2]}" style="${val[4] || ''}">
+					</div>
+				</a>
+			`;
+		} else if (val[0] === 'Sponsor') {
+			sponsorHTML += `
+				<a href="${val[3]}" target="_blank" class="sponsor-image">
+					<div>
+						<img alt="${val[1]}" src="${val[2]}" style="${val[4] || ''}">
+					</div>
+				</a>
+			`;
+		}
+	}
+
+	document.querySelector('#partners > div > div').innerHTML = partnerHTML;
+	document.querySelector('#sponsors > div > div').innerHTML = sponsorHTML;
+}
+
+function handleScheduleData(data) {
+	console.log(data);
+}
+
