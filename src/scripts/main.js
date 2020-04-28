d90286368b86c59d964031ba70598f87104752cc
Arjun Patrawala <ajpat1234@gmail.com>
2020-04-28 15:25:34 -0700

    ur welcome krish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -103,7 +103,7 @@ function handleTeamData(data) {
 		let each = data[i];
 		let html = `
 			<div class="profile">
-				<img src="${each[3]} class="lazyload" alt="${each[1]}">
+				<img src="${each[3]}" class="lazyload" alt="${each[1]}">
 				<div>
 					<h3>${each[1]}</h3>
 					<h4>${each[2]}</h4>
